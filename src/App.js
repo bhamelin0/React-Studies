24846ce855c3e673191d6587c979251f8e2edfed
bhamelin0 <bhamelin@uwo.ca>
2024-05-13 22:51:07 -0400

    Add routing and example components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Profile from './ReactFirstComponent/MyFirstComponent.js'; 
+import ProfileTwo from './ReactFirstComponent/MySecondComponent.js'; 
+
+const PAGES = { 
+  MyFirstComponent : "MyFirstComponent",
+  MySecondComponent : "MySecondComponent"
+}
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("");
+
+  function renderPage(e, pageName) {
+    e.preventDefault();
+    setCurrentPage(pageName);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Which react example do you want to view?</h1>
+      <div className="index">
+        <a className="index" href="/MyFirstComponent/index.js" onClick={(e) => renderPage(e, PAGES.MyFirstComponent)}>React First Component</a>
+        <a className="index" href="/MySecondComponent/index.js" onClick={(e) => renderPage(e, PAGES.MySecondComponent)} >React Second Component</a>
+      </div>
+
+      <Router currentPage={currentPage}/>
     </div>
+  );
+}
+
+function Router({currentPage}) {
+  return (
+    <div>
+      {
+        currentPage === PAGES.MyFirstComponent ? <Profile/> :
+        currentPage === PAGES.MySecondComponent ? <ProfileTwo/> : <div>{currentPage}</div>
+      }
+    </div>
+
   );
 }
 
